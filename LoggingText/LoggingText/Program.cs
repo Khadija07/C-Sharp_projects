e779b9aa5a0dd4af9a0a26d46a40fdd5ec606ef6
Khadijatul Kobra <ktk_01@outlook.com>
2025-02-18 19:10:25 +0200

    interface & polymorphism

---
+++ LoggingText/LoggingText/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace LoggingText;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //create folder and log text
+        string directoryPath = @"/Users/khadijatulkobra/Desktop/:Logs";
+        //string filePath = System.IO.Path.Combine(directoryPath, "log.txt");
+        string filePath = Path.Combine(directoryPath, "log.txt");
+
+        if (!Directory.Exists(directoryPath))
+        {
+            Directory.CreateDirectory(directoryPath);
+        }
+        File.AppendAllText(filePath, "Hello World!" + "\n");   
+    }
+}
