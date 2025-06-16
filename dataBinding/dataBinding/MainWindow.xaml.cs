9e1b6ffaf86254e93e0139b38734c9103af80eb6
Khadijatul Kobra <ktk_01@outlook.com>
2025-06-16 13:43:34 +0300

    data binding

---
+++ dataBinding/dataBinding/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using dataBinding.Data;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dataBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //there are three types of mode: oneway,twoway,onewaytosource. to update the property from input -> use twoway
+        Person person = new Person 
+        { 
+            //initialize obj
+            Name = "Test",
+            Age = 20
+        
+        };
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = person;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string personInfo = person.Name + " is " + person.Age + " old ";
+            MessageBox.Show(personInfo);
+        }
+    }
+}
